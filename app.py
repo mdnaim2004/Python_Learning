018a45e72c1eb4a6ae2051815890baa216a83d17
mdnaim2004 <naim.cse2004@gmail.com>
2026-04-24 17:16:17 +0600

    Add initial Python scripts with basic print statements and comments

---
+++ app.py
@@ -0,0 +1,16 @@
+print("Hi Python")
+print('Hello, Python')
+#print("Hi') # it should not working
+
+print("---------------------------------")
+print("        Learn Python             ")
+print("---------------------------------")
+
+
+
+print("                                                 ---")
+print("                                                /  _)")
+print("                                      .--^^^^--/  /  ")
+print("                                 ___/            /    ")
+print("                                 <              /     ")
+print("                                 ___.___.|_|-|_|       ")
--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
+#this is my first python code...
 print("Hello,World!\nThis is my first python code")
+
+#this is single mine comments
+#this comment not working in python
+
+x = 10 # Store the final exam score....
+
+x = 9 # this the final score
+print(x)
+
