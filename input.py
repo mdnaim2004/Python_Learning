1ad8185799536b1f51cb82497482c17db5e1aff6
Naim <naim.cse2004@gmail.com>
2026-08-13 09:56:07 +0600

    Add initial implementation for money input and total calculation in input.py; create new Jupyter notebook structure

---
+++ input.py
@@ -0,0 +1,24 @@
+# print("I need money")
+# input("I")
+# input("Give me some Money : ")
+
+# money = input("give Me Some Money : ")
+# print("the money is :", money)
+
+first_money = input("i neeed some money : ")
+second_money = input("please give me some money : ")
+
+# print(type(first_money))
+
+first_money_int = int(first_money)
+second_money_int = int(second_money)
+
+print("fist money = ", first_money_int,
+      ",and second money = ", second_money_int)
+total = first_money_int + second_money_int
+print("total money is = ", total)
+
+''' 
+hi there there is no of thid organization this is the learning python, in this python learning too this phythron
+
+'''
