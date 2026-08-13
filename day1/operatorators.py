fcf034d54bc66475ef85fc14cd66990dc7ced39e
Naim <naim.cse2004@gmail.com>
2026-08-13 10:09:42 +0600

    structure way of organiging of data

---
+++ day1/operatorators.py
@@ -0,0 +1,17 @@
+# +,-, *, / . % and etc
+# float modulas //
+
+
+num1 = 45
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+
+print(10/3)
+print(106%3) # reminder
+
+
+print(106//3)  # integer val/ purno vag
