43a6faab4a865b2719549a7b716c2243bb77f8a4
Naim <naim.cse2004@gmail.com>
2026-08-13 12:08:03 +0600

    Add comparison and while loop examples in comparisn.py and while.py; include execution outputs in basicpy.ipynb

--- day1/comparisn.py
+++ day1/comparisn.py
@@ -1,5 +1,15 @@
-num1 = 12
-num2 = 5
+# num1 = 12
+# num2 = 5
 
-power = num1**num2
-print(power)
+# power = num1**num2
+# print(power)
+
+# >, <, >=, <=, ==, !==
+# no: ++, --,
+# +=, -=, *=, /= 
+
+a = 7
+a = a+5
+
+a+= 5  #same to a=a+5
+
